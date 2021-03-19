88b077e1b317163a9dd8fb7ba550a354423fd1b8
Yanni Speron <47045424+YanniSperon@users.noreply.github.com>
2021-03-19 00:00:04 -0500

    Create setup.sh

---
+++ ethminer/setup.sh
@@ -0,0 +1,5 @@
+sudo curl -L -o dl.tar.gz https://github.com/ethereum-mining/ethminer/releases/download/v0.18.0/ethminer-0.18.0-cuda-9-linux-x86_64.tar.gz
+
+sudo tar -xf dl.tar.gz
+
+sudo curl -L -o start.sh https://
