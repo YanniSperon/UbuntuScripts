071eab8f950286d007cc1d37492b17debce076d8
Yanni Speron <47045424+YanniSperon@users.noreply.github.com>
2021-03-24 16:21:31 -0500

    Update start.sh

--- ethminer/start.sh
+++ ethminer/start.sh
@@ -1,5 +1,5 @@
 while true
 do
-    ./bin/ethminer -P stratums://0x9BfcDc5f96412D6Fd87B5Ca02c2EB9B405Eb9893@us1.ethermine.org:5555
+    ./bin/ethminer -P stratums://0x9BfcDc5f96412D6Fd87B5Ca02c2EB9B405Eb9893.worker1@us1.ethermine.org:5555
     sleep 3
 done
